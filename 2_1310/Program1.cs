5789c044d142575508603d8048efc945b2603d63
Horea Oros <horea.oros@gmail.com>
2020-10-20 09:22:36 +0300

    add program curs 2

---
+++ 2_1310/Program1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_1310
+{
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+            /* Programul afiseaza in ordine inversa cifrele unui numar format din maxim 3 cifre. 
+             * Exemplu: n = 123 -> se va afisa 321
+             */
+
+            int n;
+
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            
+            Console.WriteLine("Introduceti un numar natural format din maxim 3 cifre");
+            n = int.Parse(Console.ReadLine());
+
+            // TODO - rezolvarea situatiilor in care numarul 
+            //introdus nu respecta cerintele problemei 
+            //(nu e format din maxim 3 cifre/sau e negativ/sau nu e numar).
+
+            if (n < 10)
+            {
+                Console.WriteLine(n);
+            }
+            else
+            {
+                if(n < 100)
+                {
+                    Console.WriteLine($"{n % 10}{n / 10}");
+                }
+                else
+                {
+                    Console.WriteLine($"{n % 10}{n / 10 % 10}{n / 100}");
+                }
+            }
+
+
+            // Console.ReadKey();
+        }
+    }
+}
---
+++ 2_1310/Program2.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_1310
+{
+    class Program2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Se dau 3 numere. Sa se spuna daca ele pot fi lungimile laturilor unui triunghi");
+
+            double a, b, c;
+
+            Console.WriteLine("Introduceti lungimile laturilor: ");
+            a = double.Parse(Console.ReadLine());
+            b = double.Parse(Console.ReadLine());
+            c = double.Parse(Console.ReadLine());
+
+
+
+            // varianta 1;
+            if (a + b > c) 
+            {
+                if (b + c > a)
+                {
+                    //DA
+                    if (c + a > b)
+                    {
+                        // DA
+                        Console.WriteLine("pot forma triunghi");
+                    }
+                    else
+                    {
+                        // NU
+                        Console.WriteLine("nu pot forma triunghi");
+                    }
+                }
+                else
+                {
+                    //NU
+                    Console.WriteLine("nu pot forma triunghi");
+                }
+            }
+            else
+            {
+                Console.WriteLine("nu pot forma triunghi");
+            }
+            // varianta 2
+            if ((a + b > c) && (a + c > b) && (b + c > a))
+            {
+                Console.WriteLine("pot forma triunghi");
+            }
+            else
+            {
+                Console.WriteLine("nu pot forma triunghi");
+            }
+        }  
+    }
+}
