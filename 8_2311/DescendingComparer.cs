a7a8448c821904323d77eb9c3bf8da6058604f20
Horea Oros <horea.oros@gmail.com>
2020-11-24 10:55:20 +0200

    add week 8 project

---
+++ 8_2311/DescendingComparer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _8_2311
+{
+    internal class DescendingComparer : IComparer<int>
+    {
+        public int Compare(int x, int y)
+        {
+            if (x == y)
+            {
+                return 0;
+            }
+            else if (x > y)
+                return -1;
+            else
+                return 1;
+        }
+    }
+}
---
+++ 8_2311/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_2311
+{
+    /// <summary>
+    /// Sorting algos
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = InitArray(8, 0, 100);
+            
+            Console.WriteLine("Before...");
+            PrintArray(arr);
+
+            // Processing...
+            //Array.Sort(arr, new DescendingComparer());
+
+
+            //Console.WriteLine("Insertion Sorting...");
+            //InsertionSort(arr);
+
+            Console.WriteLine("Selection Sorting...");
+            SelectionSort(arr);
+
+            Console.WriteLine("After...");
+            PrintArray(arr);
+        }
+
+        private static void SelectionSort(int[] arr)
+        {
+            int aux;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int k = i;
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[j] < arr [k])
+                    {
+                        k = j;
+                    }
+                }
+                aux = arr[k];
+                arr[k] = arr[i];
+                arr[i] = aux;
+            }
+        }
+
+        private static void InsertionSort(int[] arr)
+        {
+            int aux;
+            for (int i = 1; i < arr.Length; i++)
+            {
+                for (int k = i; k > 0 && arr[k] < arr[k - 1]; k--)
+                {
+                    aux = arr[k];
+                    arr[k] = arr[k - 1];
+                    arr[k - 1] = aux;
+                }
+                // invariant: elementele vectorului de la pozitia 0 pana la i 
+                // sunt in ordine crescatoare. 
+            }
+        }
+
+        private static void PrintArray(int[] arr)
+        {
+            foreach (int item in arr)
+            {
+                Console.Write($"{item} ");
+            }
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// Aloca memorie pentru un vector si initializeaza elementele vectorului 
+        /// v cu valori aleatoare din interalul [min, max)
+        /// </summary>
+        /// <param name="length">lungimea vectorului care este initializat</param>
+        /// <param name="min">Limita inferioara inclusiv</param>
+        /// <param name="max">Limita superioara exclusiv</param>
+        private static int[] InitArray(int length, int min, int max)
+        {
+            int[] v = new int[length];
+            Random rnd = new Random();
+            for (int i = 0; i < v.Length; i++)
+            {
+                v[i] = rnd.Next(min, max);
+            }
+            return v;
+        }
+    }
+}
